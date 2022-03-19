566651fba58bab9e3761420733cb48fbe708dec1
EmmaDeste <emma_deste@hotmail.com>
2022-03-19 18:58:10 +0100

    Upgrade: pilotage par résistances et transistor

--- source/1_blinkLED/1_blinkLED.ino
+++ source/1_blinkLED/1_blinkLED.ino
@@ -1,18 +1,27 @@
-#define PINLED 13
+//blink LED using 1MO and 220kO resistances, a transistor
 
-void setup() //exécuter qu'une fois au démarrage
+#define REDLEDPIN 13
+#define YELLOWLEDPIN 17 //correspondant au pin A3 = D17 de l'Arduino Nano 
+
+void setup() //exécuter qu'une fois au démarrage 
 {
-  //dire que cette PIN est utilisée en sortie
-  pinMode(PINLED, OUTPUT);
+  delay(1000);
+  //dire que cette PIN est utilisée en sortie 
+  pinMode(REDLEDPIN, OUTPUT);
+  pinMode(YELLOWLEDPIN, OUTPUT);
 
 }
 
 void loop() //exécuter en boucle
 {
-  //allumer la LED, attendre 1s, puis l'éteindre, attendre 1s, puis la rallumer, ...
-  digitalWrite(PINLED, HIGH);
+  //alternance d'allumage
+  digitalWrite(REDLEDPIN, HIGH);
   delay(1000);
-  digitalWrite(PINLED, LOW);
+  digitalWrite(YELLOWLEDPIN, LOW);
+  delay(1000);
+  digitalWrite(REDLEDPIN, LOW);
+  delay(1000);
+  digitalWrite(YELLOWLEDPIN, HIGH);
   delay(1000);
 
 }
